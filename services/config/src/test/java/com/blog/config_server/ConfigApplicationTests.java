71ba635a662dc0381893e105ab74176cd748beb1
Shreyash Tekade <shreyashtekade2503@gmail.com>
2025-01-03 15:51:01 +0530

    Added the config and discovery server

---
+++ services/config/src/test/java/com/blog/config_server/ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ services/discovery/src/test/java/com/blog/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
