7c15769bf6e9f456458f8f2f51e9a7526d803de7
Shreyash Tekade <shreyashtekade2503@gmail.com>
2025-01-16 23:04:36 +0530

    Added the blog crud microservice

---
+++ services/crud/src/main/java/com/blog/crud/blog/Blog.java
@@ -0,0 +1,39 @@
+package com.blog.crud.blog;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Blog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "blog_seq")
+    @SequenceGenerator(name = "product_seq", sequenceName = "blog_sequence", allocationSize = 50)
+    private Integer id;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    private String author;
+
+    @Lob
+    @Column(nullable = false)
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime lastModifiedAt;
+}
---
+++ services/crud/src/main/java/com/blog/crud/blog/Category.java
@@ -0,0 +1,31 @@
+package com.blog.crud.blog;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "category_seq")
+    @SequenceGenerator(name = "category_seq", sequenceName = "category_sequence", allocationSize = 50)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    @OneToMany(mappedBy = "category")
+    private List<Blog> blogs;
+}
