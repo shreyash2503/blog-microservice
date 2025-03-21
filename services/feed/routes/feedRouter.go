a19faa0d96d22b577b3f28b29f835d3e22b84eb3
Shreyash Tekade <shreyashtekade2503@gmail.com>
2025-03-21 23:20:47 +0530

    Added get feeds route

--- services/feed/routes/feedRouter.go
+++ services/feed/routes/feedRouter.go
@@ -2,6 +2,8 @@ package routes
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/shreyash2503/blog-feed/controllers"
+	"github.com/shreyash2503/blog-feed/db"
 	"github.com/shreyash2503/blog-feed/middleware"
 )
 
@@ -9,10 +11,17 @@ func FeedRoutes(router *gin.Engine) {
 	router.Use(middleware.Authenticate)
 
 	router.GET("/feed", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "Sending feed successfully",
-		})
+		username, _ := c.Get("username")
+		blogs, err := controllers.GetFeed(username.(string), db.DB)
+		if err != nil {
+			c.JSON(500, gin.H{
+				"error": "Internal server error",
+			})
+		}
 
+		c.JSON(200, gin.H{
+			"data": blogs,
+		})
 	})
 
 }
